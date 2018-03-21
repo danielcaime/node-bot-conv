46d54f28494f86608444d5feaa059c8057084e87
caime.daniel <caime.daniel@outlook.com>
2018-03-21 10:28:55 -0300

    app secret

--- app.js
+++ app.js
@@ -35,13 +35,13 @@ const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
   config.get('appSecret');
 
 // Arbitrary value used to validate a webhook
-const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
-  (process.env.MESSENGER_VALIDATION_TOKEN) :
+const VALIDATION_TOKEN = (process.env.WEBHOOK_TOKEN) ?
+  (process.env.WEBHOOK_TOKEN) :
   config.get('validationToken');
 
 // Generate a page access token for your page from the App Dashboard
-const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
-  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+const PAGE_ACCESS_TOKEN = (process.env.PAGE_ACCESS_TOKEN) ?
+  (process.env.PAGE_ACCESS_TOKEN) :
   config.get('pageAccessToken');
 
 // URL where the app is running (include protocol). Used to point to scripts and 
